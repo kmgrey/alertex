210933de61a3cfaf045075e1fda81a0a2753c104
cmykatie <kathryncreek@gmail.com>
2024-01-23 21:13:29 +0000

    Make burger nav responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("burger-nav").addEventListener("click", () => {
+    const burgerPop = document.getElementById("burger-popout");
+    if (burgerPop.style.maxHeight === "0px" || burgerPop.style.maxHeight === "") {
+        burgerPop.style.display = "flex";
+        burgerPop.style.maxHeight = burgerPop.scrollHeight + "px";
+    } else {
+        burgerPop.style.maxHeight = "0";
+        setTimeout(() => {
+            burgerPop.style.display = "none";
+        }, 350);
+    }
+});
