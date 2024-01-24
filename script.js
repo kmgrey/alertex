0e954021ec0de6f4621cb584ee5374ba89a0eefe
cmykatie <kathryncreek@gmail.com>
2024-01-24 21:29:42 +0000

    Change images, header, and footer

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.getElementById("burger-nav").addEventListener("click", () => {
-    const burgerPop = document.getElementById("burger-popout");
-    if (burgerPop.style.maxHeight === "0px" || burgerPop.style.maxHeight === "") {
-        burgerPop.style.display = "flex";
-        burgerPop.style.maxHeight = burgerPop.scrollHeight + "px";
-    } else {
-        burgerPop.style.maxHeight = "0";
-        setTimeout(() => {
-            burgerPop.style.display = "none";
-        }, 350);
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("burger-nav").addEventListener("click", () => {
+        const burgerPop = document.getElementById("burger-popout");
+        if (burgerPop.style.maxHeight === "0px" || burgerPop.style.maxHeight === "") {
+            burgerPop.style.display = "flex";
+            burgerPop.style.maxHeight = burgerPop.scrollHeight + "px";
+        } else {
+            burgerPop.style.maxHeight = "0";
+            setTimeout(() => {
+                burgerPop.style.display = "none";
+            }, 350);
+        }
+    });
 });
