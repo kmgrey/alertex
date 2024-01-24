398429b5f82a8e63bcd5ea971e4f7595053275fc
Kathryn <kathryncreek@gmail.com>
2024-01-24 12:45:00 +0000

    Test

---
+++ header.php
@@ -0,0 +1,26 @@
+<!-- L E V E L  1  H E A D E R -->
+<header>
+    <nav>
+        <div id="left-nav">
+            <!-- <a class="nav-link" href="about.html">About Us</a> -->
+            <a class="nav-link" href="./products.html">Products</a>
+            <a class="nav-link" href="./approved-installers.html">Approved Installers</a>
+        </div>
+        <div id="center-nav">
+            <a href="./index.html">
+                <div class="image-container">
+                    <img id="alertex-nav-logo" src="./images/logos/alertex-logo.png" alt="Alertex Wireless Critical Alert System logo">
+                </div>
+            </a>
+        </div>
+        <div id="burger-nav">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>
+        </div>
+        <div id="right-nav">
+            <a class="nav-link" href="./faq.html">FAQ</a>
+            <a class="nav-link" href="./contact.html">Contact Us</a>
+        </div>
+    </nav>
+</header>
